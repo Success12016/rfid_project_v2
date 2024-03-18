72a5d6ed2be1fbbb969d1ef102d0f082e29dcf41
Success12016 <pattarachai12016@gmail.com>
2024-03-18 22:53:57 +0700

    navbar example

--- local/src/App.jsx
+++ local/src/App.jsx
@@ -2,6 +2,10 @@ import { useEffect, useState } from 'react';
 import './App.css';
 import axios from 'axios'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
 function App() {
   const [goldData, setGoldData] = useState([])
@@ -12,7 +16,7 @@ function App() {
   }, [])
 
 return(
-  <div className="w-100 vh-100 d-flex justify-content-center align-items-center">
+  /*<div className="w-100 vh-100 d-flex justify-content-center align-items-center">
     <div className="w-50">
     <table className="table">
       <thead>
@@ -45,7 +49,30 @@ return(
       </tbody>
     </table>
     </div>
-  </div>
+  </div>*/
+  <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
 );
 }
 
